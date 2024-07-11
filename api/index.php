8c38544507271bafdc80c1f2c9929f395e033eb5
irmaosdev1 <161535246+irmaosdev1@users.noreply.github.com>
2024-07-11 14:09:34 -0300

    Alterações index

--- api/index.php
+++ api/index.php
@@ -1,3 +1,5 @@
-<?php declare(strict_types = 1);
+<?php 
+// declare(strict_types = 1);
 // api/index.php
-phpinfo(INFO_ALL & ~INFO_ENVIRONMENT & ~INFO_CONFIGURATION & ~INFO_VARIABLES);
+phpinfo();
+// INFO_ALL & ~INFO_ENVIRONMENT & ~INFO_CONFIGURATION & ~INFO_VARIABLES
